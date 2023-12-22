738464bfcecdef44d31bcb3af288532e13c1e167
Ritik Raj <ritikraj1875@gmail.com>
2023-12-22 21:00:08 +0530

    ip

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser =require('body-parser');
 const jwt =require('jsonwebtoken');
 
-mongoose.connect('mongodb://localhost:27017/BITKART');
+mongoose.connect('mongodb://127.0.0.1:27017/BITKART');
 const Users = mongoose.model('Users',{
     username : String ,
     password: String,
