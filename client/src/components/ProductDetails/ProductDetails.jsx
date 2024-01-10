d50dc76934dd8773cca264b8198988134434da20
Ritik Raj <ritikraj1875@gmail.com>
2024-01-10 13:15:10 +0530

    123

--- client/src/components/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -2,75 +2,91 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Nav from "../Home/Nav/Nav";
-
-function ProductDetail(){
+import "./ProductDetails.css";
+import  whatsapp from "./whatsapp.png"
+function ProductDetail() {
     const p = useParams();
     console.log(p.productId);
-    const [product,setproduct] =useState();
-    const[user,setUser]=useState();
+    const [product, setproduct] = useState();
+    const [user, setUser] = useState();
     console.log(user);
 
-    useEffect(()=>{
-        const url ='http://localhost:4000/getProduct/'+p.productId;
+    useEffect(() => {
+        const url = 'http://localhost:4000/getProduct/' + p.productId;
         axios.get(url)
-        .then((res)=>{
-            if(res.data.products){
-                setproduct(res.data.products);
-                console.log(res);
-            }
-            
-        })
-        .catch((err)=>{
-            console.log(err);
-            alert('server err');
-        })
+            .then((res) => {
+                if (res.data.products) {
+                    setproduct(res.data.products);
+                    console.log(res);
+                }
 
-    },[]);
-  const  HandleContact=(addedBy)=>{
-    const url ='http://localhost:4000/getUser/'+addedBy;
+            })
+            .catch((err) => {
+                console.log(err);
+                alert('server err');
+            })
+
+    }, []);
+    const HandleContact = (addedBy) => {
+        const url = 'http://localhost:4000/getUser/' + addedBy;
         axios.get(url)
-        .then((res)=>{
-            if(res.data.user){
-                setUser(res.data.user);
-            }  
-        })
-        .catch((err)=>{
-            console.log(err);
-            alert('server err');
-        })
+            .then((res) => {
+                if (res.data.user) {
+                    setUser(res.data.user);
+                }
 
-        
+
+
+            })
+            .catch((err) => {
+                console.log(err);
+                alert('server err');
+            })
+
+
     }
+    let uu = user && user.phno ? `https://wa.me/+91${user.phno}` : '';
 
-    return(<>
+
+    return (<>
         <Nav></Nav>
+        <div className="page-conatiner">
+            <div className="product-detail-container">
             <h2>PRODUCT DETAILS:</h2>
-             <div className="">
-            
-             {product && <div>
-                    <div>
-                        <img src={"http://localhost:4000/"+product.Pimage }alt="" />
-                        {product.Pimage2&&<img src={"http://localhost:4000/"+product.Pimage2 }alt="" />}
-                        <h6>Product Description:</h6>
-                        {product.ProductDesc}
-                    </div>
-                    <div>
-                      {product.ProductName}
-                      {product.ProductPrice}
-                    </div>
-                    <div>
-                        {product.addedBy&& <button onClick={()=>HandleContact(product.addedBy)}>Show Contact Details:</button>}
-                        {user && user.username &&  <h2>Username:{user.username}</h2>}
-                        {user && user.username &&  <h2>Phone Number:{user.phno}</h2>}
-                        {user && user.username &&  <h2>Email:{user.email}</h2>}
-                        {user && user.username &&  <h2>Hostel Number:{user.hno}</h2>}
-                    </div>
+                    {product &&<div className="product-details">
+                       <div className="product-images">
+                            <img src={"http://localhost:4000/" + product.Pimage} alt="" />
+                            {product.Pimage2 && <img src={"http://localhost:4000/" + product.Pimage2} alt="" />}
+                            
+                        </div>
+                       <div className="pdd">
+                      <div className="product-info">
+                           <h2> Product Name:{product.ProductName}</h2> 
+                           <h2> Product Price: Rs{product.ProductPrice} </h2>
+                           <h2>Prodcut Description:</h2> <p> {product.ProductDesc}</p>
+                           
+                        </div>
+                        <div className="contact-details">
+                            {product.addedBy && <button className="pd" onClick={() => HandleContact(product.addedBy)}>Show Contact Details:</button>}
+                            {user && user.username && <h2>Username:{user.username}</h2>}
+                            {user && user.phno && <h2>Phone Number:{user.phno}</h2>}
+                            {user && user.username && <h2>Email:{user.email}</h2>}
+                            {user && user.hno && <h2>Hostel Number:{user.hno}</h2>}
+                            {user && user.roll && <h2>Roll:{user.roll}</h2>}
+                            {user && user.year && <h2>Year:{user.year}</h2>}
+                            {user && user.branch && <h2>Branch:{user.year}</h2>}
+                            {user && user.phno && <a href={uu}><img src={whatsapp} alt="" /></a>}
+                              
+                        </div>
+                       
+                       </div>
+                    </div>}
 
-                 </div>}
-                 
 
+              
+            </div>
         </div>
-        </>
+    </>
     );
 }
 export default ProductDetail;
